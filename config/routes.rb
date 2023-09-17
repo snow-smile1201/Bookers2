b27c8d4c16c5b146d329c9a875af2e5ee4171547
MasayukiHorimoto <141377771+snow-smile1201@users.noreply.github.com>
2023-09-17 04:02:09 +0000

    [Add]Bootstrap導入、userコントローラー、booksコントローラー、ビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/index'
+  get 'users/edit'
+  get 'users/show'
+  get 'books/new'
+  get 'books/index'
+  get 'books/edit'
+  get 'books/show'
   devise_for :users
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
