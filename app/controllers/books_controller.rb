651fd844d814480c6a1f447559e73f007f9706e1
MasayukiHorimoto <141377771+snow-smile1201@users.noreply.github.com>
2023-09-17 05:40:46 +0000

    [Add]books index view作成途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,49 @@
 class BooksController < ApplicationController
-  def new
-  end
-
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
-  def edit
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       flash[:notice] = "Book was successfully updated."
+       redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to '/books'
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/index'
-  get 'users/edit'
-  get 'users/show'
-  get 'books/new'
-  get 'books/index'
-  get 'books/edit'
-  get 'books/show'
   devise_for :users
   root to: 'homes#top'
+  resources :books, only: [:new, :index, :show, :edit, :create, :destroy]
   get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
