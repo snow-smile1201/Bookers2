4058642165f55a88551fad9d6fb2f0fb90878c59
MasayukiHorimoto <141377771+snow-smile1201@users.noreply.github.com>
2023-09-18 09:37:42 +0000

    [Add]book index,user index, user showページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,49 +1,38 @@
 class BooksController < ApplicationController
   def index
-    @book = Book.new
+    @user = current_user
     @books = Book.all
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
-      flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book.id)
-    else
-      @books = Book.all
-      render :index
-    end
+    @newbook = Book.new(book_params)
+    @newbook.user_id = current_user.id
+    @newbook.save
+    redirect_to users_path
   end
 
   def show
-    @book = Book.find(params[:id])
+
   end
 
   def edit
-    @book = Book.find(params[:id])
+
   end
 
   def update
-    @book = Book.find(params[:id])
-    if @book.update(book_params)
-       flash[:notice] = "Book was successfully updated."
-       redirect_to book_path(@book.id)
-    else
-      render :edit
-    end
+
   end
 
   def destroy
-    @book = Book.find(params[:id])
-    @book.destroy
-    flash[:notice] = "Book was successfully destroyed."
-    redirect_to '/books'
+
   end
 
   private
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+  def user_params
+    params.require(:user).permit(:id, :name, :introduction)
   end
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,42 @@
 class UsersController < ApplicationController
-  def new
-  end
 
   def index
+    @user = current_user
+    @books = Book.all
+    @users = User.all
   end
 
-  def edit
+  def create
+    
   end
 
   def show
+    @user = current_user
+    @books = @user.books
+
+  end
+
+  def edit
+    @user = current_user
+
+  end
+
+  def update
+    @user = current_user
+
+  end
+
+  def destroy
+
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  def user_params
+    params.require(:user).permit(:id, :name, :introduction)
   end
 end
+
+
