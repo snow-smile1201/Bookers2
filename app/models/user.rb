2d524a7e52b6bf97cbd5aadac8bca2bbe89dcbde
MasayukiHorimoto <141377771+snow-smile1201@users.noreply.github.com>
2023-10-05 09:44:04 +0000

    閲覧数カウント機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,9 +16,11 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followings, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
-  
+
   has_many :messages, dependent: :destroy
   has_many :entries, dependent: :destroy
+
+  has_many :view_counts, dependent: :destroy
 
   has_one_attached :profile_image
 
