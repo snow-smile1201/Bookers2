45ed00af9ad118d662dc71ac485b7d07b9b8f012
MasayukiHorimoto <141377771+snow-smile1201@users.noreply.github.com>
2023-10-10 04:29:54 +0000

    ビューの表示修正、DM機能エラー修正

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,7 +2,7 @@ class RoomsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @room = Room.create
+    @room = Room.create(user_id: current_user.id)
     @entry1 = Entry.create(room_id: @room.id, user_id: current_user.id)
     @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id))
     redirect_to "/rooms/#{@room.id}"
